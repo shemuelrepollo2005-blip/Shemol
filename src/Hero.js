4c3780fb99a432e541f7c1dd35eac3c0809b29ca
Shemuel Repollo <shemuelrepollo2005@gmail.com>
2026-09-24 12:35:54 +0800

    Updated Hero section

--- src/Hero.js
+++ src/Hero.js
@@ -5,7 +5,7 @@ function Hero() {
  <h1>Hello! I'm Shemuel S. Repollo</h1>
  <p>Welcome to my portfolio website.</p>
  <div style={{ marginTop: '20px' }}>
- <img src="/10218e972f966c2ac150d89c3781d7ed.jpg" alt="Profile" />
+ <img src="/10218e972f966c2ac150d89c3781d7ed.jpg" alt="Profile" width="100px"/>
  </div>
  </section>
  );
